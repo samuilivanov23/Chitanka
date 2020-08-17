68641df32873cfe7866b6d1d377b2eedfd8582c7
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-08-17 01:18:12 -0700

    Removing unnecessa code

--- unique_words.py
+++ unique_words.py
@@ -1,7 +1,6 @@
 import re, os
 
 my_dirnames = os.listdir("../books")
-unique_words = []
 words_count = dict()
 count = 0
 
@@ -17,19 +16,13 @@ for my_dir in my_dirnames:
 
             current_file_words = list(set(re.findall("[а-яА-Я]{3,}", file_content)))
 
-            f = open("../unique_words.txt", encoding="utf-8", mode="w+")
             for word in current_file_words:
                 word = word.lower()
-                f.write(word+"\n")
                 if word in words_count:
                     words_count[word] += 1
                 else:
                     words_count[word] = 1
             f.close()
-
-f = open("../unique_words.txt", encoding='utf-8', mode='r')
-file_content = f.read()
-f.close()
 
 for key in words_count:
     if words_count[key] == 1:
