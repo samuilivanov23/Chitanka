4d1c6674ca63e6cbb80a0fb757a24d3bd1a80636
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-08-19 14:19:23 -0700

    Removing unused code from filter_words.py

--- filter_words.py
+++ filter_words.py
@@ -2,33 +2,22 @@ f = open("../unique_words_9.txt", encoding='utf-8', mode='r')
 file_content = f.read()
 f.close()
 word_list = file_content.split("\n")
-word_list_len = len(word_list)
 
 final_words = []
-
-start_index = 0
-end_index = 2000
 count = 0
-current_file_words = file_content.split("\n")[start_index:end_index]
 
-while count < word_list_len:
-    for word in current_file_words:
-        if word[:(len(word) - 1)] in word_list:
-            final_words.append(word[:(len(word) - 1)])
-        elif word[:(len(word) - 2)] in word_list:
-            final_words.append(word[:(len(word) - 2)])
-        elif word[:(len(word) - 3)] in word_list:
-            final_words.append(word[:(len(word) - 3)])
-        else:
-            final_words.append(word)
+for word in word_list:
+    if word[:(len(word) - 1)] in word_list:
+        final_words.append(word[:(len(word) - 1)])
+    elif word[:(len(word) - 2)] in word_list:
+        final_words.append(word[:(len(word) - 2)])
+    elif word[:(len(word) - 3)] in word_list:
+        final_words.append(word[:(len(word) - 3)])
+    else:
+        final_words.append(word)
 
-        print(count)
-        count+=1
-    
-    start_index+=2000
-    end_index+=2000
-    current_file_words = file_content.split("\n")[start_index:end_index]
-    
+    print(count)
+    count+=1
 
 final_words = list(set(final_words))
 
